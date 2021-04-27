806b1822dc59e81ab2d5a145f6f3d735157083d8
Alfandavi Aryo Utomo <alfanau18@gmail.com>
2021-04-27 14:52:38 +0700

    as
    as

--- dist/database.js
+++ dist/database.js
@@ -1,43 +1,43 @@
 let mysql = require('mysql');
 
 const createDatabase = () => {
-  let con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root"
-  });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE stima", function (err, result) {
-      if (err) throw err;
-      console.log("Database created");
+    let con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "123456"
     });
-  });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        con.query("CREATE DATABASE stima", function(err, result) {
+            if (err) throw err;
+            console.log("Database created");
+        });
+    });
 }
 
 const createTable = () => {
-  let con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "stima"
-  });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = "CREATE TABLE jadwal (id INT AUTO_INCREMENT PRIMARY KEY, tanggal DATE, kode VARCHAR(255), nama_tugas VARCHAR(255), deskripsi VARCHAR(255))";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
+    let con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "123456",
+        database: "stima"
     });
-  });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = "CREATE TABLE jadwal (id INT AUTO_INCREMENT PRIMARY KEY, tanggal DATE, kode VARCHAR(255), nama_tugas VARCHAR(255), deskripsi VARCHAR(255))";
+        con.query(sql, function(err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        });
+    });
 }
 
 
 module.exports = {
-  createDatabase: createDatabase,
-  createTable: createTable
+    createDatabase: createDatabase,
+    createTable: createTable
 }
